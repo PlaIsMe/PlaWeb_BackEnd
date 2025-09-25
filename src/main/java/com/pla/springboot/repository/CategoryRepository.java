3b524ad7b8e72ef8911289aa5df76302bafc3fdc
PlaIsMe <97893528+PlaIsMe@users.noreply.github.com>
2025-09-25 15:34:08 +0700

    Update CategoryRepository.java add Search, Pagination, Post

--- src/main/java/com/pla/springboot/repository/CategoryRepository.java
+++ src/main/java/com/pla/springboot/repository/CategoryRepository.java
@@ -1,8 +1,11 @@
 package com.pla.springboot.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pla.springboot.entity.Category;
@@ -12,4 +15,14 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
 
     Optional<Category> findByName(String name);
+
+    @Query("SELECT c FROM Category c " +
+            "WHERE c.name " +
+            "LIKE %?1% ")
+    List<Category> search(String kw, Pageable pageable);
+
+    @Query("SELECT COUNT(*) FROM Category c " +
+            "WHERE c.name " +
+            "LIKE %?1% ")
+    Long count(String kw);
 }
